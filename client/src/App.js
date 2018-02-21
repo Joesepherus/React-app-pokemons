d13b0e33445430049c9c434b3c0fe65a7350ec26
Jozef Maloch <Jozefmaloch@gmail.com>
2018-02-22 00:42:18 +0100

    first 9 pokemons are being now displayed with their images also they are in good order, it was a pain to get it working asychronously because of all the 9 request calls from pokeapi

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = { pokemons: [{
-  }] }
+  state = {
+    pokemons: [{
+      name: "",
+      sprites: ""
+    }],
+    all: [{
 
-  componentDidMount(){
-    fetch('/api/pokemons')
-      .then(res => res.json())
-      .then(pokemons => this.setState({ 
-          pokemons
-      }))
-      .then(console.log(this.state.pokemons))
+    }]
   }
+
+  componentDidMount() {
+      fetch('/api/pokemons')
+        .then((response) => response.json())
+        .then(pokemons => this.setState({
+          pokemons: pokemons
+        }))
+        .then(console.log(this.state.pokemons))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Pokemons</h1>
-          <ul>
-            { this.state.pokemons.map(pokemon =>
-              <li key={pokemon.id}>{pokemon.name}</li>
-            )}
-          </ul>
+        <ul>
+          {this.state.pokemons.map(pokemon =>
+            <li>{pokemon.name}
+            <img src={pokemon.sprites.front_default}></img>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -9,29 +9,46 @@ app.use(express.static(__dirname + '/client'));
 const env = process.env.NODE_ENV || 'development';
 var db = mongoose.connection;
 var Pokemon = require('./client/src/models/pokemon.js');
-
 // DB SETUP
 MONGOLAB_URI = process.env.MONGOLAB_URI_MATHWEB;
 
 mongoose.connect(MONGOLAB_URI, function (error) {
-	if (error) console.error(error);
+  if (error) console.error(error);
 });
 
-// display all problems
-app.get('/api/pokemons', function(req, res){
-  request('http://pokeapi.co/api/v2/pokemon/', function (error, response, body){
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body)
-      console.log(info.results);
-      res.send(info.results);
-    }
+function getEm(i) {
+  return new Promise((resolve, reject) => {
+    request('http://pokeapi.co/api/v2/pokemon/' + i, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        console.log(info.name);
+        resolve(info);
+        //res.send(info);
+      }
+    })
   })
+}
+
+// display all problems
+app.get('/api/pokemons', function (req, res) {
+  var all = [];
+  for (var i = 1; i < 10; i++) {
+    all.push(getEm(i));
+
+  }
+  setTimeout(function () {
+    Promise.all(all).then(function (values) {
+      console.log(values);
+      res.send(values);
+    });
+    //console.log(all.Promise.resolve());
+  }, 15000);
 })
 
 // calling server to listen on port
 var server = app.listen(process.env.PORT || 3001, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log("App listening at http://%s:%s", host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("App listening at http://%s:%s", host, port);
 })
 // --------------------------------------------------------------------------------------------
